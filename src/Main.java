2dc2fde26ecc6d111ec2f33f515c1b3783b00f94
Kaveh Shahhosseini <kaveh228@gmail.com>
2019-08-17 18:06:22 +0430

    New Functions Added
    Compare Two Tree
    Find Max Value in Tree
    Find Min Value in Tree

--- src/Main.java
+++ src/Main.java
@@ -2,18 +2,38 @@
 public class Main {
 
     public static void main(String[] args) {
-        Tree T = new Tree();
+        Tree T1 = new Tree();
         Node r = new Node(49);
-        T.addNode(r);
-        T.addNode(new Node(50));
-        T.addNode(new Node(25));
-        T.addNode(new Node(18));
-        T.addNode(new Node(14));
-        T.addNode(new Node(36));
-        System.out.println(T.nodeCount);
-        T.inOrderPrint();
-        T.preOrderPrint();
-        T.postOrderPrint();
+        T1.addNode(r);
+        T1.addNode(new Node(50));
+        T1.addNode(new Node(25));
+        T1.addNode(new Node(18));
+        T1.addNode(new Node(14));
+        T1.addNode(new Node(36));
+        System.out.println("\n"+T1.nodeCount);
+        T1.inOrderPrint();
+        T1.preOrderPrint();
+        T1.postOrderPrint();
+        T1.levelOrderPrint();
+        T1.getMaxValue();
+        T1.getMinValue();
+
+        Tree T2 = new Tree();
+        T2.addNode(new Node(10));
+        T2.addNode(new Node(8));
+        T2.addNode(new Node(25));
+        T2.addNode(new Node(16));
+        T2.addNode(new Node(5));
+        T2.addNode(new Node(4));
+        T2.addNode(new Node(6));
+        System.out.println("\n"+T2.nodeCount);
+        T2.inOrderPrint();
+        T2.preOrderPrint();
+        T2.postOrderPrint();
+        T2.levelOrderPrint();
+        T2.getMaxValue();
+        T2.getMinValue();
+        System.out.println("\n"+Tree.isEqual(T1.root,T2.root));
 
     }
 }
--- src/Tree.java
+++ src/Tree.java
@@ -1,6 +1,6 @@
 
 class Tree {
-    private Node root;
+    Node root;
     int nodeCount;
 
     void addNode(Node node) {
@@ -28,10 +28,12 @@ class Tree {
 
     void inOrderPrint() {
         System.out.print("\ninOrder Traversal: \n");
+        if(root==null) return;
         inOrder(root);
     }
 
     private void inOrder(Node root) {
+
         if(root.left!= null)
             inOrder(root.left);
         System.out.print(root.value+" ");
@@ -41,6 +43,7 @@ class Tree {
     }
     void preOrderPrint() {
         System.out.print("\npreOrder Traversal: \n");
+        if(root==null) return;
         preOrder(root);
     }
 
@@ -55,6 +58,7 @@ class Tree {
     }
     void postOrderPrint() {
         System.out.print("\npostOrder Traversal: \n");
+        if(root==null) return;
         postOrder(root);
     }
 
@@ -67,4 +71,65 @@ class Tree {
         }
         System.out.print(root.value+" ");
     }
+    void levelOrderPrint() {
+        System.out.print("\nlevelOrder Traversal: \n");
+        if(root==null) return;
+        levelOrder(root);
+    }
+
+    private void levelOrder(Node root) {
+        //TODO
+        if (root.left != null) {
+            postOrder(root.left);
+        }
+        if (root.right != null) {
+            postOrder(root.right);
+        }
+        System.out.print(root.value+" ");
+    }
+
+    void getMaxValue(){
+        if(root==null) System.out.print("\nNo Node Available");
+        System.out.print("\nMax Value: "+MaxValue(root));
+    }
+    private int MaxValue(Node root){
+        if(root.right!=null){
+            return (MaxValue(root.right));
+        }
+        return root.value;
+    }
+    void getMinValue(){
+        if(root==null) System.out.print("\nNo Node Available");
+        System.out.print("\nMin Value: "+MinValue(root));
+    }
+    private int MinValue(Node root){
+        if(root.left!=null){
+            return (MinValue(root.left));
+        }
+        return root.value;
+    }
+
+    static boolean isEqual(Node root1, Node root2){
+        if(root1==null && root2==null)
+            return true;
+        if(root1==null || root2==null)
+            return false;
+
+        return (root1.value==root2.value
+                    && isEqual(root1.left,root2.left)
+                    && isEqual(root1.right, root2.right));
+    }
+    
+    boolean isBST(){ //Binary Search Tree
+        //TODO
+        return true;
+    }
+    int findLCA(){ //Lowest common ancestor
+        //TODO
+        return 0;
+    }
+    void deleteNode(Node root, int value){
+        //TODO
+    }
+
 }
